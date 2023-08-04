74702eae8042967ebd4d9d72a67c1c93f8827785
Jhafet19 <20223tn107@utez.edu.mx>
2023-08-04 11:04:54 -0600

    arreglado

--- Integradora/src/main/java/mx/edu/utez/integradora/controllers/Stories/ServletStories.java
+++ Integradora/src/main/java/mx/edu/utez/integradora/controllers/Stories/ServletStories.java
@@ -5,7 +5,10 @@ import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import jakarta.servlet.http.HttpSession;
+import mx.edu.utez.integradora.models.stories.Categories;
 import mx.edu.utez.integradora.models.stories.Stories;
+import mx.edu.utez.integradora.models.user.Status;
+import mx.edu.utez.integradora.models.user.User;
 
 import java.io.IOException;
 
@@ -17,6 +20,8 @@ import java.io.IOException;
 public class ServletStories extends HttpServlet {
     String action, redirect ="/api/auth2";
     Stories story;
+    private Status status;
+    private Categories categories;
     HttpSession session;
     String id, title, content, created_atDATETIME,file, status_id, user_id, category_id;
 
@@ -34,10 +39,50 @@ public class ServletStories extends HttpServlet {
         req.setCharacterEncoding("UTF-8");
         resp.setCharacterEncoding("UTF-8");
         resp.setContentType("text/html");
+
         action = req.getServletPath();
+
         System.out.println("post");
+
+        switch(action){
+            case "/api/story/save":
+                try {
+                    title = req.getParameter("title");
+                    content = req.getParameter("content");
+                    file = req.getParameter("file");
+                    user_id = req.getParameter("user_id");
+
+                    Status status1 = new Status();
+//                    status = "3"; //status publicado
+//                    status1.setType_status(status);
+                    User user1 = new User();
+
+//                    categories = req.getParameter("categories");
+
+                    story = new Stories();
+                    story.setId(0L);
+                    story.setTitle(title);
+                    story.setContent(content);
+                    //story.setFile(byte[] file); // pendiente pk no le sabemos
+                    story.setStatus(status1);
+                    //  story.setUser_id(User user_id);
+                    //       story.setCategories(Categories categories);
+
+
+
+                    /*id, title content, created_atDATETIME, file
+                     * status, user, categories*/
+
+                }catch (Exception e){
+
+                }
+                break;
+
+        }
+
+//>>>>>>> 5b132da2375c229295a9aef189a06702cb4ffb7e
     }
-}
+    }
 
 
 
